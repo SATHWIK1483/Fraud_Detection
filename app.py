8573a473c52098f184974dc687ab7a84b6419431
Sathwik <117582084+SATHWIK1483@users.noreply.github.com>
2025-03-02 17:47:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,22 +10,21 @@ with open('final_model.sav', 'rb') as model_file:
 st.title("Financial Transaction Fraud Prediction System")
 st.subheader("Enter Transaction Details")
 
-# User inputs
+# User Inputs
 TransactionAmt = st.number_input("Transaction Amount (USD)", min_value=0.0, step=0.01)
 ProductCD = st.text_input("Product Code")
 card1 = st.number_input("Payment Card 1", min_value=0, step=1)
 card2 = st.number_input("Payment Card 2", min_value=0, step=1)
 card3 = st.number_input("Payment Card 3", min_value=0, step=1)
-card4 = st.selectbox("Payment Card Type", ["Credit", "Debit", "Other"])
+card4 = st.number_input("Payment Card Type", min_value=0, step=1)
 card5 = st.number_input("Payment Card 5", min_value=0, step=1)
-card6 = st.selectbox("Payment Card Category", ["1", "2", "3", "4"])
+card6 = st.number_input("Payment Card Category", min_value=0, step=1)
 addr1 = st.number_input("Billing Address 1", min_value=0, step=1)
-addr2 = st.number_input("Billing Address 2", min_value=0, step=1)
 P_emaildomain = st.text_input("Primary Email Domain")
 R_emaildomain = st.text_input("Recipient Email Domain")
 
 # Prepare input data
-input_data = np.array([[TransactionAmt, card1, card2, card3, card5, addr1, addr2]])
+input_data = np.array([[TransactionAmt, card1, card2, card3, card4, card5, card6, addr1]])
 
 # Make prediction
 if st.button("Predict Fraudulent Transaction"):
